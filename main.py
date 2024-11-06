90fce1dd9258bfacc5b5fb2762796d80695cf296
melodious12 <zjulian10@gmail.com>
2024-11-06 16:55:02 -0500

    updated main.py to count words in texts

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def word_count(text):
+    words = text.split()
+    count = len(words)
+    print(count)
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
+        word_count(file_contents)
 main()
